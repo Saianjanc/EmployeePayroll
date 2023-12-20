e308c8d360b8c8ddaa34e2ae5303ce81b8fc7c91
Saianjan <saianjan1234@gmail.com>
2023-12-20 16:20:37 +0530

    MODIFIED:Added Employee Login Page

---
+++ js/EmployeeDetails.js
@@ -0,0 +1,45 @@
+class EmpolyeeDetails{
+    get name(){
+        return this.empName
+    }
+    set name(empName){
+        this.empName=empName
+    }
+    // get profileImg(){
+    //     return this.empImg
+    // }
+    // set profileImg(empImg){
+        
+    // }
+    // get gender(){
+    //     return this.empGender
+    // }
+    // set gender(empGender){
+        
+    // }
+    // get dept(){
+    //     return this.empDept
+    // }
+    // set dept(empDept){
+        
+    // }
+    // get salary(){
+    //     return this.empSalary
+    // }
+    // set salary(empSalary){
+        
+    // }
+    // get startDate(){
+    //     return this.empStartDate
+    // }
+    // set startDate(empStartDate){
+        
+    // }
+    // get note(){
+    //     return this.empNote
+    // }
+    // set note(empNote){
+        
+    // }
+}
+export default EmpolyeeDetails;
